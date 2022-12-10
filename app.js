9ccfcae38072a36e6fba73beb4b06c71435072b3
Web__Dev <murod.london@gmail.com>
2022-12-10 13:39:36 +0000

    Accordion created JS logic different.

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const accordion = document.getElementsByClassName(
-  'content-container',
+const openBtn = Array.from(
+  document.querySelectorAll('.question'),
+);
+const closeBtn = Array.from(
+  document.querySelectorAll('.answer'),
 );
 
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active');
+openBtn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    closeBtn.forEach((close) => {
+      if (
+        e.target.nextElementSibling !== close &&
+        close.classList.contains('active')
+      ) {
+        close.classList.remove('active');
+        openBtn.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+      }
+    });
+
+    const panel = btn.nextElementSibling;
+    panel.classList.toggle('active');
+    btn.classList.toggle('active');
   });
-}
+});
+
+window.onclick = (e) => {
+  if (!e.target.matches('.question')) {
+    openBtn.forEach((btn) =>
+      btn.classList.remove('active'),
+    );
+    closeBtn.forEach((btn) =>
+      btn.classList.remove('active'),
+    );
+  }
+};
